e42f709bbcdfe70b3396b6241bf937425d4b6393
Andrii Falieiev <falieievandrii@gmail.com>
2022-08-20 21:47:09 +0300

    deposit calculator, final edition

---
+++ deposit-calculator/js/task1.js
@@ -0,0 +1,47 @@
+alert('Welcome to deposit calculator!' + 
+'\nTap OK to start immediately!');
+
+//in loops was used two types of using assignment operators, for a test purpose.
+let moneyAmount = parseFloat(prompt('Input amount of money, that you want to invest,' +
+' it should be more or equal 1000$ (don\'t use $ sign, type only a number):', ''));
+while (moneyAmount !== true || moneyAmount < 1000) {
+  alert('Invalid input data, tap OK and try again.');
+  moneyAmount = parseFloat(prompt('Input correct initial amount, that you want to invest,' +
+  ' it should be more or equal 1000 (don\'t use $ sign, type only a number):', ''));
+}
+
+
+let depositYears = Math.round(prompt('Input desired investment period in years.' + 
+' It should be an integer, one year or more (float number will be rounded):', ''));
+while (!depositYears || depositYears < 1) {
+  alert('Invalid input data, tap OK and try again.');
+  depositYears = Math.round(prompt('Input correct investment period in years.' +
+  ' It should be an integer, one year or more (float number will be rounded):', ''));
+}
+
+let percentageYear = parseFloat(prompt('Input percentage for your deposit per year,' +
+' it should be not more than 100% (don\'t use % sign, type only a number):', ''));
+while (!percentageYear || percentageYear > 100) {
+  alert('Invalid input data, tap OK and try again.');
+  percentageYear = parseFloat(prompt('Input correct percentage for your deposit per year,' +
+  ' it should be not more than 100% (don\'t use % sign, type only a number):', ''));
+}
+
+let totalAmount = moneyAmount;
+for (let i = 0; i < depositYears; i++) {
+  totalAmount = (totalAmount * (1 + percentageYear / 100)).toFixed(2);
+}
+
+let totalProfit;
+totalProfit = (totalAmount - moneyAmount).toFixed(2);
+
+//here was used two types of writing output (string + variable), just for a test purpose.
+alert(
+  `Your investment: ${moneyAmount}$` +
+    '\nInvestment period in years: ' +
+    depositYears +
+    `\nPercentage per year: ${percentageYear}%` +
+    '\n\nTotal amount: ' +
+    totalAmount + '$' +
+    `\nTotal profit: ${totalProfit}$`
+);
