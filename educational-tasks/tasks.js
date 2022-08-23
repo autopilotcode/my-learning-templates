d9004b4036c0a05e36aee8c03a2b76613771dcdc
Andrii Falieiev <falieievandrii@gmail.com>
2022-08-23 13:12:49 +0300

    isBigger

--- educational-tasks/tasks.js
+++ educational-tasks/tasks.js
@@ -7,7 +7,7 @@
 // For example:
 // isEquals(3, 3) // => true
 
-function isEquals(a,b) {
+const isEquals = (a,b) => {
     // if (a === b) {
     //     return true;
     // } else {
@@ -17,3 +17,21 @@ function isEquals(a,b) {
 }
 
 console.log(isEquals(3, 3));
+
+
+// //Task2
+// Write a function - isBigger
+// It should accept two arguments and returns true if first one has greater value than second one or
+// false otherwise.
+// Tip: no need for if/else clause nor ternary operator
+// For example:
+// isBigger(5, -1) // => true
+// repeat: ('приведение типов') js (type coercion)
+
+const isBigger = (a,b) => {
+    return a > b;
+}
+
+console.log(isBigger(5, -1));
+
+
