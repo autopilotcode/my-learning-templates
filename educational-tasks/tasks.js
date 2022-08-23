c0e4a4cc83f3204ec2ae87ddf05bfefd9b47d88f
Andrii Falieiev <falieievandrii@gmail.com>
2022-08-23 12:43:18 +0300

    function - isEqual

---
+++ educational-tasks/tasks.js
@@ -0,0 +1,19 @@
+//HW5
+// Task #1
+// Write a function - isEquals
+// It should accept two arguments and returns true if first one value equals second one or false
+// otherwise.
+// Tip: no need for if/else clause nor ternary operator
+// For example:
+// isEquals(3, 3) // => true
+
+function isEquals(a,b) {
+    // if (a === b) {
+    //     return true;
+    // } else {
+    //     return false;
+    // }
+    return a === b;
+}
+
+console.log(isEquals(3, 3));
