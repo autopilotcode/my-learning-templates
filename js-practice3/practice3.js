4883b3d3d47531275a0951c011d936b34bedce52
Andrii Falieiev <falieievandrii@gmail.com>
2022-09-01 10:47:19 +0300

    current1

--- js-practice3/practice3.js
+++ js-practice3/practice3.js
@@ -1,49 +1,82 @@
-//Async Code
-console.log('ASYNC CODE');
+// //Async Code
+// console.log('ASYNC CODE');
 
-console.log('1---------setTimeout')
+// console.log('1---------setTimeout')
+
+// // setTimeout(() => {
+// //     console.log('Timeout 2 sec');
+// // }, 2000);
 
 // setTimeout(() => {
-//     console.log('Timeout 2 sec');
-// }, 2000);
-
-setTimeout(() => {
-    console.log('Timeout is not defined (sync code always run first)');
-});
+//     console.log('Timeout is not defined (sync code always run first)');
+// });
 
 
-console.log('2---------clearTimeout');
+// console.log('2---------clearTimeout');
 
-const timerId = setTimeout(() => {
-    console.log('Asynchronous');
-});
-clearTimeout(timerId);
-//no output
+// const timerId = setTimeout(() => {
+//     console.log('Asynchronous');
+// });
+// clearTimeout(timerId);
+// //no output
 
-const timerId1 = setTimeout(() => {
-    console.log('Asynchronous, with 0 delay');
-})
+// const timerId1 = setTimeout(() => {
+//     console.log('Asynchronous, with 0 delay');
+// })
 
-setTimeout(() => {
-    clearTimeout(timerId);
-}, 1); //upper block will be processed and after 1ms timeout will be cleared
+// setTimeout(() => {
+//     clearTimeout(timerId);
+// }, 1); //upper block will be processed and after 1ms timeout will be cleared
 
 
 
-console.log('3---------set / clear interval');
+// console.log('3---------set / clear interval');
 
-const makeCounter = () => {
-    let count = 0;
+// const makeCounter = () => {
+//     let count = 0;
 
-    return () => {
-        console.log(count);
-        count += 1;
+//     return () => {
+//         console.log(count);
+//         count += 1;
+// }
+// }
+
+// const intervalId = setInterval(makeCounter(), 1000);
+
+// setTimeout(() => {
+//     clearInterval(intervalId);
+// }, 5000);
+
+console.log('3-----------------------callback');
+let c;
+
+const testFunc1 = (a,b) => {
+    c = a + b;
+    testCallBack1(c);
 }
+
+const testCallBack1 = (c) => {
+    console.log(c);
 }
 
-const intervalId = setInterval(makeCounter(), 1000);
+testFunc1(5,5);
 
-setTimeout(() => {
-    clearInterval(intervalId);
-}, 5000);
 
+
+
+console.log('3---------set timeout & callback');
+      
+const main = () => {
+      callback();
+}
+
+const callback = () => {
+    console.log('Success - callback');
+    setTimeout(callback1, 3000);
+}
+
+const callback1 = () => {
+    console.log('Success - callback1');
+}
+
+setTimeout(main, 3000);
